f68d57c55bfab22f36fc5d70df2476c88206955f
PeterKokenyessy <kokenypeter2001@gmail.com>
2026-03-17 12:05:04 +0100

    fix mapping

--- src/main/java/hu/budgetflix/api/mapper/MovieMapper.java
+++ src/main/java/hu/budgetflix/api/mapper/MovieMapper.java
@@ -3,8 +3,11 @@ package hu.budgetflix.api.mapper;
 import hu.budgetflix.api.model.dto.MovieDto;
 import hu.budgetflix.api.model.entity.Movie;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface MovieMapper {
+
+    @Mapping(target = "title", source = "title")
     MovieDto toDto(Movie movie);
 }
