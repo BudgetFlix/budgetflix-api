511cf7f3656ac44ec82b20af76ecb3c77b7851fe
PeterKokenyessy <kokenypeter2001@gmail.com>
2026-03-17 13:50:14 +0100

    add stream controller

---
+++ src/main/java/hu/budgetflix/api/controller/StreamController.java
@@ -0,0 +1,19 @@
+package hu.budgetflix.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/stream")
+public class StreamController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Void> stream (@PathVariable Long id){
+        return ResponseEntity.ok()
+                .header("X-Accel-Redirect","/internal/media" + id + "/hls/index.m3u8")
+                .build();
+    }
+}
