3e45aa576f5f978270ef10c9ea3cdd2c68ba87ab
PeterKokenyessy <kokenypeter2001@gmail.com>
2026-03-19 11:23:10 +0100

    change mapper to class and add id to dto

--- src/main/java/hu/budgetflix/api/mapper/MovieMapper.java
+++ src/main/java/hu/budgetflix/api/mapper/MovieMapper.java
@@ -2,12 +2,12 @@ package hu.budgetflix.api.mapper;
 
 import hu.budgetflix.api.model.dto.MovieDto;
 import hu.budgetflix.api.model.entity.Movie;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
-public interface MovieMapper {
+@Component
+public class MovieMapper {
 
-    @Mapping(target = "title", source = "title")
-    MovieDto toDto(Movie movie);
+    public MovieDto toDto(Movie movie){
+        return new MovieDto(movie.getTitle(), movie.getId());
+    }
 }
--- src/main/java/hu/budgetflix/api/model/dto/MovieDto.java
+++ src/main/java/hu/budgetflix/api/model/dto/MovieDto.java
@@ -1,4 +1,4 @@
 package hu.budgetflix.api.model.dto;
 
-public record MovieDto(String title) {
+public record MovieDto(String title, Long id ) {
 }
